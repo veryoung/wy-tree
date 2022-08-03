812bfeea42f87c8c7ae17452b088b2ad2f5fec81
veryoung <675874828@qq.com>
2022-08-03 10:27:56 +0800

    Update index.js

--- src/components/tree/index.js
+++ src/components/tree/index.js
@@ -1,6 +1,4 @@
-/**
- * Created by ayou on 17/7/21.
- */
+
 exports.VueTreeList = require('./VueTreeList.vue')
 exports.TreeNode = require('./Tree.js').TreeNode
 exports.Tree = require('./Tree.js').Tree
